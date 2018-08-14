5a38ce9c53c6299928bda91ed6ad4c5a7f86ac2e
Jonbi2 <marcellobardus8@gmail.com>
2018-08-14 13:46:30 +0200

    ds18b20 hotfix

--- SensorsAPI/app/routesMethods/DS18B20.py
+++ SensorsAPI/app/routesMethods/DS18B20.py
@@ -6,6 +6,12 @@ import time
 
 
 def ds18b20_get_data(device_id=None ,time_range_begin=None, time_range_end=None):
+    
+    if time_range_begin is None and time_range_end is None and device_id is None:
+            result = ds18b20_DbClient.select_data()
+            result = jsonify(result)
+            return result
+
     if time_range_begin is int and time_range_end is int:  # timestamp is int !
         where_query = "WHERE timestamp > " + str(time_range_begin) + " AND timestamp < " + str(time_range_end) + " AND sensor_id = " + str(device_id)
         result =  ds18b20_DbClient.select_data("*", where_query)
@@ -23,10 +29,6 @@ def ds18b20_get_data(device_id=None ,time_range_begin=None, time_range_end=None)
         result = jsonify(result)
         return result
 
-    if time_range_begin is None and time_range_end is None and device_id is None:
-        result = ds18b20_DbClient.select_data()
-        result = jsonify(result)
-        return result
 
     return jsonify({'error': 'wrong arguments have been given'})
 
