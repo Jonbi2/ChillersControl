9fea2b897eac2be8fc91720667669f7d1e99f4e8
Jonbi2 <marcellobardus8@gmail.com>
2018-08-14 13:20:29 +0200

    Debugging selecting data from SQLite

--- SensorsAPI/app/routesMethods/DS18B20.py
+++ SensorsAPI/app/routesMethods/DS18B20.py
@@ -5,7 +5,7 @@ from flask import jsonify
 import time
 
 
-def ds18b20_get_data(device_id ,time_range_begin=None, time_range_end=None):
+def ds18b20_get_data(device_id=None ,time_range_begin=None, time_range_end=None):
     if time_range_begin is int and time_range_end is int:  # timestamp is int !
         where_query = "WHERE timestamp > " + str(time_range_begin) + " AND timestamp < " + str(time_range_end) + " AND sensor_id = " + str(device_id)
         result =  ds18b20_DbClient.select_data("*", where_query)
@@ -20,6 +20,11 @@ def ds18b20_get_data(device_id ,time_range_begin=None, time_range_end=None):
     if time_range_end is None:
         where_query = "WHERE timestamp > " + str(time_range_begin) + " AND timestamp < " + str(round(time.time())) + " AND sensor_id = " + str(device_id)
         result =  ds18b20_DbClient.select_data("*", where_query)
+        result = jsonify(result)
+        return result
+
+    if time_range_begin is None and time_range_end is None and device_id is None:
+        result = ds18b20_DbClient.select_data()
         result = jsonify(result)
         return result
 
