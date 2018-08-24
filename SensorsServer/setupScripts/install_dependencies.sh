cc3de6eb4e1ca43961ef5bfab17384d5dcdf45b8
Jonbi2 <marcellobardus8@gmail.com>
2018-08-24 11:44:20 +0200

    hotfix

--- SensorsServer/setupScripts/install_dependencies.sh
+++ SensorsServer/setupScripts/install_dependencies.sh
@@ -4,6 +4,7 @@ sudo apt-get install build-essential
 sudo apt-get install python3
 sudo apt-get install pip3
 sudo apt-get install virtualenv
+sudo apt-get -y install python3-rpi.gpio
 sudo apt-get install screen
 
 cd .. 
@@ -22,5 +23,6 @@ pip3 install flask_restful
 pip3 install multitasking
 pip3 install w1thermsensor
 pip3 install flask_cors
+sudo pip3 install RPi.GPIO
 
 cd setupScripts/
