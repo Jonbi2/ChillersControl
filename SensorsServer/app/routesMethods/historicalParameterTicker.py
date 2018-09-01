9cf21bacaea23bec82a24afd7f5406a7fa14401a
Jonbi2 <marcellobardus8@gmail.com>
2018-09-01 17:58:24 +0200

    Testing

--- SensorsServer/app/routesMethods/historicalParameterTicker.py
+++ SensorsServer/app/routesMethods/historicalParameterTicker.py
@@ -173,7 +173,7 @@ def get_historical_ticker(timerange_begin=None, csv=None):
         i_json['date'] = times[i][0]
         i_json['timestamp'] = times[i][1]
         result.append(i_json)
-        print(i)
+        print(i, times[i])
     if csv is None:
         return jsonify(result)
     else:
