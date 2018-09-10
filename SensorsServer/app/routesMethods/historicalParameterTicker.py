9c4c7ad53fa2fa3d29beb1ece32364cf6f7431e8
Jonbi2 <marcellobardus8@gmail.com>
2018-09-10 17:44:19 +0200

    Hotfix

--- SensorsServer/app/routesMethods/historicalParameterTicker.py
+++ SensorsServer/app/routesMethods/historicalParameterTicker.py
@@ -133,6 +133,7 @@ def get_historical_ticker(timerange_begin=None, csv=None):
     sql_query = "SELECT date, timestamp FROM flow_meters_readings WHERE timestamp > " + str(timerange_begin)
     try:
         times = list(flow_meter_DbClient.session.execute(sql_query).fetchall())
+        print(times[len(times) - 1])
     except OperationalError:
         time.sleep(0.01)
         return get_historical_ticker(timerange_begin, csv)
